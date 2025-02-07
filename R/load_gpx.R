e9429368576843cd512fee236128c191a22a86e0
Russ <russshean@gmail.com>
2025-02-06 17:44:11 -0800

    fix function documentation

--- R/load_gpx.R
+++ R/load_gpx.R
@@ -1,17 +1,17 @@
-#' Title
+#' Load a .gpx file from disk
 #'
 #' @description
-#' This function allows you to vectorise multiple [switch()] statements. Each
-#' case is evaluated sequentially and the first match for each element
-#' determines the corresponding value in the output vector. If no cases match,
-#' the `.default` is used.
+#' This function loads a locally stored .gpx file using the track_points layer 
+#' and returns a simple features (sf) data frame
+#' 
 #'
 #' @param path A character vector with one element. This should be the path to a locally stored gpx file.
 #'
 #' @return A dataframe
 #' @export
 #'
-#' @examples   
+#' @examples
+#'activity1 <- load_gpx("C:/Users/your user name/activities/activity1.gpx")
 
 load_gpx <- function(path){
   
--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # stravinator
 Introducing the stravinator! An R package to process your gpx files like strava does 
+<!-- badges: start -->
+  [![R-CMD-check](https://github.com/Russell-Shean/stravinator/actions/workflows/R-CMD-check.yaml/badge.svg)](https://github.com/Russell-Shean/stravinator/actions/workflows/R-CMD-check.yaml)
+  [![Codecov test coverage](https://codecov.io/gh/Russell-Shean/stravinator/graph/badge.svg)](https://app.codecov.io/gh/Russell-Shean/stravinator)
+<!-- badges: end -->
 
 # Setup     
 To create the package and GitHub repo I followed these <a href="https://r-pkgs.org/whole-game.html">instructions</a>
