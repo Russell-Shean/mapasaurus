8bd2827f6229c174d424b269026b14888be5159d
Russ <russshean@gmail.com>
2025-02-06 21:49:33 -0800

    futz with function documentation instead of actually building anything

--- R/load_gpx.R
+++ R/load_gpx.R
@@ -1,11 +1,11 @@
 #' Load .gpx files from disk
 #'
 #' @description
-#' This function loads a locally stored .gpx file and returns a dataframe. 
+#' This function loads a locally stored .gpx file and returns a simple features (sf) dataframe. 
 #'
 #' @param path A character vector with one element. This should be the path to a locally stored gpx file.
 #'
-#' @return A dataframe
+#' @return A simple features (sf) dataframe
 #' @export
 #' @examples   
 #' activity1 <- load_gpx("C:/Users/my_user_name/activities/activity1.gpx")
