cf6f7007465ca96262fcc52dfd75108902e061ed
Russ <russshean@gmail.com>
2025-02-06 18:25:25 -0800

    add tests

--- R/load_gpx.R
+++ R/load_gpx.R
@@ -1,22 +1,18 @@
-#' Load a .gpx file from disk
+#' Load .gpx files from disk
 #'
 #' @description
-#' This function loads a locally stored .gpx file using the track_points layer 
-#' and returns a simple features (sf) data frame
-#' 
+#' This function loads a locally stored .gpx file and returns a dataframe. 
 #'
 #' @param path A character vector with one element. This should be the path to a locally stored gpx file.
 #'
 #' @return A dataframe
 #' @export
-#'
-#' @examples
-#'activity1 <- load_gpx("C:/Users/your user name/activities/activity1.gpx")
+#' @examples   
+#' activity1 <- load_gpx("C:/Users/my_user_name/activities/activity1.gpx")
 
 load_gpx <- function(path){
   
-  gpx <- sf::st_read(path,layer="track_points") 
   
-  return(gpx)
+  print(path)
   
 }
