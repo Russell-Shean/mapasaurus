2001570bbae6857b4c2152965fdbef502d97ff4d
Russell Shean <73149125+Russell-Shean@users.noreply.github.com>
2025-02-09 11:33:02 -0800

    add libraries to shiny app
    hopefully this doesn't mess up the package part of the repo lol....

--- inst/route-explorer-app/ui.R
+++ inst/route-explorer-app/ui.R
@@ -1,3 +1,12 @@
+# add libraries to shiny app
+if(!require("pacman")){install.packages("pacman")}
+pacman::p_load(dplyr,
+               leaflet,
+               sf,
+               shiny)
+
+
+
 ui <- fluidPage(
   titlePanel("GPX File Upload"),
   gpxUploadUI("gpxUploader")
